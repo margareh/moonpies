bc8201b6571b99e35e1a2fcff51a5bb789df96e4
Maggie Hansen <margareh@andrew.cmu.edu>
2025-06-24 12:16:16 -0400

    Update GP model of mixing ratio and ejecta temp

--- mixing_ratio_ej_temp_gp.py
+++ mixing_ratio_ej_temp_gp.py
@@ -2,8 +2,10 @@
 GP for interpolating between mixing ratios and ejecta temps
 """
 
+import os
 import pandas as pd
 import numpy as np
+import matplotlib.pyplot as plt
 
 import gpytorch
 import torch
@@ -34,18 +36,20 @@ def load_data(file):
     xx = xx.reshape((nx*ny))
     yy = yy.reshape((nx*ny))
     zz = data.values.astype(np.float64).reshape((nx*ny))
-    return xx, yy, zz
+    return xx, yy, zz, nx, ny
 
 
 # fit a GP
 def fit_gp(xx, yy, zz, iters=1000):
 
     # set up data
-    train_x = np.vstack(xx, yy)
+    train_x = np.vstack((xx, yy)).T
+    train_x_t = torch.Tensor(train_x)
+    train_z_t = torch.Tensor(zz)
 
     # set up model
     lik = gpytorch.likelihoods.GaussianLikelihood()
-    mod = GP(train_x, zz, lik)
+    mod = GP(train_x_t, train_z_t, lik)
     lik.train()
     mod.train()
 
@@ -56,25 +60,84 @@ def fit_gp(xx, yy, zz, iters=1000):
     # iterate
     for i in range(iters):
         opt.zero_grad()
-        out = mod(train_x)
-        loss = -mll(out, zz)
+        out = mod(train_x_t)
+        loss = -mll(out, train_z_t)
         loss.backward()
+        if i % 100 == 0:
+            print("Iter %d: loss = %5.4f" % (i, loss))
         opt.step()
 
-    return mod
+    return mod, lik
+
+
+# plot some things from the GP
+def plot_gp(xx, yy, zz, gp_mod, gp_lik, nx, ny):
+
+    gp_mod.eval()
+    gp_lik.eval()
+
+    # predict from the gp over the training set
+    train_x = torch.Tensor(np.vstack((xx, yy)).T)
+    with torch.no_grad():
+        zz_pred = gp_lik(gp_mod(train_x))
+    zz_mean = zz_pred.mean.numpy()
+    zz_var = zz_pred.variance.numpy()
+    error = (zz_mean - zz)**2
+    print("MSE (Var SE): %5.4f (%5.4f)" % (np.mean(error), np.var(error)))
+
+    # create plots
+    fig, ax = plt.subplots(2, 2, figsize=(20,20))
+
+    # actual and error
+    im1 = ax[0,0].imshow(zz.reshape((nx, ny)), cmap='plasma')
+    im2 = ax[0,1].imshow(error.reshape((nx, ny)), cmap='coolwarm')
+    ax[0,0].set_title('Actual')
+    ax[0,1].set_title('Error')
+    fig.colorbar(im1, ax=ax[0,0])
+    fig.colorbar(im2, ax=ax[0,1])
+
+    # predicted mean and uncertainty
+    im3 = ax[1,0].imshow(zz_mean.reshape((nx, ny)), cmap='plasma')
+    im4 = ax[1,1].imshow(zz_var.reshape((nx, ny)), cmap='Reds')
+    ax[1,0].set_title('Predicted Mean')
+    ax[1,1].set_title('Predicted Uncertainty')
+    fig.colorbar(im3, ax=ax[1,0])
+    fig.colorbar(im4, ax=ax[1,1])
+
+    # plt.show()
+    plt.savefig('/home/margareh/moonpies/moonpies/data/gp_preds.png', dpi=100, bbox_inches='tight')
+    plt.close()
 
 
 
 if __name__ == "__main__":
 
+    # file paths
+    inpath = '/home/margareh/moonpies/moonpies/data/ballistic_sed_frac_melted_mean.csv'
+    outpath = '/home/margareh/moonpies/moonpies/data/ballistic_sed_frac_melted_mean_gp.pth'
+
     # load the data
-    xx, yy, zz = load_data('/home/margareh/moonpies/moonpies/data/ballistic_sed_frac_melted_mean.csv')
+    xx, yy, zz, nx, ny = load_data(inpath)
 
-    # fit the GP
-    gp = fit_gp(xx, yy, zz, iters=1000)
+    if os.path.exists(outpath) == False:
 
-    # save the GP model
-    torch.save(gp.state_dict(), '/home/margareh/moonpies/moonpies/data/ballistic_sed_frac_melted_mean_gp.pth')
+        # fit the GP
+        gp_mod, gp_lik = fit_gp(xx, yy, zz, iters=1000)
 
-    # show some interpolation plots
+        # save the GP model
+        torch.save(gp_mod.state_dict(), outpath)
+    
+    else:
+
+        # load the model
+        state_dict = torch.load(outpath)
+        gp_lik = gpytorch.likelihoods.GaussianLikelihood()
+        train_x = np.vstack((xx, yy)).T
+        train_x_t = torch.Tensor(train_x)
+        train_z_t = torch.Tensor(zz)
+        gp_mod = GP(train_x_t, train_z_t, gp_lik)
+        gp_mod.load_state_dict(state_dict)
+
+    # plot some things
+    plot_gp(xx, yy, zz, gp_mod, gp_lik, nx, ny)
 
