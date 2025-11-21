Fix sign

moonpies/utils/utils.py
@@ -222,7 +222,7 @@ def get_grid_arrays(cfg, half=False):
     xsize, xstep = cfg.grdxsize, cfg.grdstep
     if half:
         grdy, grdx = np.meshgrid(
-            np.arange(-ysize, 0, -ystep, dtype=cfg.dtype),
+            np.arange(ysize, 0, -ystep, dtype=cfg.dtype),
             np.arange(0, xsize, xstep, dtype=cfg.dtype),
             sparse=True,
             indexing='ij'